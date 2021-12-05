a47b4d8aeb9413ab62e1117e9eba4215d3d0c4a4
proteus21 <94799210+proteus21@users.noreply.github.com>
2021-12-05 18:25:23 +0100

    Update Study_4.py

--- Homework/Study_4.py
+++ Homework/Study_4.py
@@ -1,11 +1,11 @@
 #Question:
-#Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-#between 2000 and 3200 (both included).
+#Write a program which will find all such numbers which are divisible by 9 but are not a multiple of 8,
+#between 1000 and 5200 (both included).
 #The numbers obtained should be printed in a comma-separated sequence on a single line.
 
 l=[]
-for i in range(2000, 3201):
-    if (i%7==0) and (i%5!=0):
+for i in range(1000, 5201):
+    if (i%9==0) and (i%8!=0):
         l.append(str(i))
 
 print(l)
